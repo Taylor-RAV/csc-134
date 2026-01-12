46f9b42f1f5fb4282434b84e4f97386f1b0d8a6a
Taylor-RAV <taylorj9680@student.faytechcc.edu>
2026-01-12 16:35:48 +0000

    finished M1T1

---
+++ M1T1-Taylor.cpp
@@ -0,0 +1,12 @@
+// CSC 134
+// M1T1, WORLD!
+
+// MAGIC WORDS
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello CSC 134!" <<endl; //E N D L
+    cout << endl; // another new line
+    return 0; // no errors
+}
