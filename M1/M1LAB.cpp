ad7a8af91c3c5844a4b2df1c01b3997fd1cb46c5
Taylor-RAV <taylorj9680@student.faytechcc.edu>
2026-01-16 02:47:26 +0000

    finished M1LAB

--- M1/M1LAB.cpp
+++ M1/M1LAB.cpp
@@ -13,9 +13,16 @@ int apples = 100;
 // price per apple 
 double pricePerApple = 0.25; 
 // calculate the total price of the apples 
-// TODO 
+double totalprice = apples * pricePerApple;
 // print all the information about the orchard 
-// TODO 
 cout << "Welcome to " << name; 
 cout << "’s apple orchard." << endl; 
+cout << "We have " << apples;
+cout << " apples in stock." << endl;
+cout << "Apples are currently $";
+cout << pricePerApple <<" each." << endl;
+cout << endl;
+cout << "To buy all apples it would cost $";
+cout << totalprice <<" total." << endl;
+ cout << endl;
 }
