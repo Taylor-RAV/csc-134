40cfda62dd3fd1282bac24256a773d7a845e36f2
Taylor-RAV <taylorj9680@student.faytechcc.edu>
2026-01-21 16:11:44 +0000

    save awaiting imput for changes

---
+++ M1/M1HW1.cpp
@@ -0,0 +1,27 @@
+// CSC 134 
+// M1LAB 
+// Your Name 
+// The Date 
+#include <iostream> 
+using namespace std; 
+int main() { 
+// This program will simulate an apple orchard. 
+// The owner’s name 
+string name = "\"The Princess Bride\" "; 
+string date = "1987 "; 
+string quote1 = "Inigo Montoya: Hello. My name is Inigo Montoya. You killed my father. Prepare to die."; 
+string quote2 = "[Vizzini has just cut the rope The Dread Pirate Roberts is climbing up] Vizzini: HE DIDN'T FALL? INCONCEIVABLE! Inigo Montoya: You keep using that word. I do not think it means what you think it means.";
+string gross = "30.8 million.";
+
+cout << "The movie " << name; 
+cout << "came out in " << date; 
+cout << "and grossed " << gross <<endl;
+cout << endl;
+cout << "    The American critic Dalton Mullins praised the film as one of the best love stories ever filmed, writing: \"The chemistry between the two leads is palpable and is clear from the first moment they're on screen together. When Buttercup asks Wesley [sic] to do something, the tension of the unspoken love between the two is profound and dense, especially when they were standing face to face right before they professed their undying love for each other.\" Mullins argued that the phrase \"as you wish\" sums up the film's philosophy as Westley was willing to suffer any hardship and brave any peril for the woman he loves, which led him to argue the film was a story about \"true love\".  Mullins, Dalton (February 3, 2020). \"As You Wish\": True Love and Longing in The Princess Bride\". Cineccentric. Archived from the original on March 26, 2023. " << endl;
+cout << endl;
+cout << "Quote 1" << endl;
+cout << quote1 << endl;
+cout << endl;
+cout << "Quote 2" << endl;
+cout << quote2 << endl;
+}
