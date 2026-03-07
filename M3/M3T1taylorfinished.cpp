498fc26c3245277c7b8dcea048528b3c36b1b8d8
Taylor-RAV <taylorj9680@student.faytechcc.edu>
2026-03-07 20:27:55 +0000

    finished m3t1

--- M3/M3T1taylorfinished.cpp
+++ M3/M3T1taylorfinished.cpp
@@ -1,7 +1,7 @@
 // CSC 134
 // M3T1 
-// norrisa
-// 2/17/2025
+// James Taylor
+// 3/7/2026
 // Ask the user for the width and length of two rectangles
 // Find the area
 
@@ -26,8 +26,15 @@ int main() {
     cin >> wid2;
 
     // Find the area
+    area1 = len1 * wid1;
+    area2 = len2 * wid2;
+    double total_area = area1 + area2;
 
     // Print the area
-
+    cout << "Area of first rectangle:        " << area1 << endl;
+    cout << "Area of second rectangle:       " << area2 << endl;
+    cout << "Total area of both rectangles:  " << total_area << endl;
+    cout << endl;
+    cout << endl;
     return 0;
 }
